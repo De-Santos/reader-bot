feed171d2cdc13707efa41807757251e47e416ac
Misha <miha.18012006@gmail.com>
2023-08-07 22:38:01 +0200

    --init--

---
+++ src/test/java/com/hard/reader/bot/ReaderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hard.reader.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
